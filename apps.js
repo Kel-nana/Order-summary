9a0010a0f3158ae5ddb984bcf98a22b1907dfa7a
kelvinirungu <kelvinn.irungu69@gmail.com>
2022-09-06 22:20:05 +0300

    close button html/css/js

--- apps.js
+++ apps.js
@@ -1,7 +1,26 @@
-const buttons = document.querySelectorAll(".btn");
+//REFERENCES
+const buttons = document.querySelectorAll("button");
+const popup = document.querySelector('.popup-cancel')
+const close = document.querySelector('.fa-window-close')
+//POP
 
-buttons.forEach(button => {
-button.addEventListener("click", (e) => {
-e.target.classList.toggle("btn-clicked");
+buttons.forEach((button, index) => {
+    console.log(index);
+    if (index===1){
+        button.addEventListener("click", (e) => {
+           popup.style.display ="block";
+             
+            });
+    }
+
 });
-});
+
+// =========CLOSE BUTTON=======
+
+
+    close.addEventListener("click", (e) => {
+    popup.style.display ="none";
+             
+    });
+    
+
